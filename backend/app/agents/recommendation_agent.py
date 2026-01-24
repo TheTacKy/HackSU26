c6b9a3d527e912eb1e66c914e84141a57ed6ed89
joshishruti9 <joshishruti009@gmail.com>
2026-01-24 14:31:08 -0800

    Filtering changes to show desired result

--- backend/app/agents/recommendation_agent.py
+++ backend/app/agents/recommendation_agent.py
@@ -3,13 +3,11 @@ def generate_recommendations(repos, issues_map):
 
 
     for repo in repos:
-        key = repo["full_name"]
         recommendations.append({
         "name": repo["name"],
         "url": repo["html_url"],
         "stars": repo["stargazers_count"],
         "description": repo["description"],
-        "issues": issues_map.get(key, [])
         })
 
 
